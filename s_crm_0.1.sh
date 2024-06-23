ce9b5f868984d83aae0eb07ae135314926baaa70
motaviegas <motaviegas@gmail.com>
2024-06-23 14:35:12 +0100

    Update s_crm_0.1.sh

--- s_crm_0.1.sh
+++ s_crm_0.1.sh
@@ -15,7 +15,7 @@ server_ip=$(get_input "Digite o IP do servidor")
 echo "Atualizando e instalando pacotes..."
 sudo add-apt-repository ppa:ondrej/php -y && sudo apt update && sudo apt upgrade -y
 
-sudo apt update && sudo apt install php8.2 libapache2-mod-php8.2 php8.2-cli php8.2-curl php8.2-common php8.2-intl php8.2-gd php8.2-mbstring php8.2-mysqli php8.2-pdo php8.2-mysql php8.2-xml php8.2-zip php8.2-imap php8.2-ldap -y php8.2-curl php8.2-soap php8.2-bcmath
+sudo apt update && sudo apt install unzip php8.2 libapache2-mod-php8.2 php8.2-cli php8.2-curl php8.2-common php8.2-intl php8.2-gd php8.2-mbstring php8.2-mysqli php8.2-pdo php8.2-mysql php8.2-xml php8.2-zip php8.2-imap php8.2-ldap -y php8.2-curl php8.2-soap php8.2-bcmath
 
 # Configurar Apache
 echo "Configurando Apache..."
